5fd5d8f8d2951a96bfdfea952a4a097ccba02dc8
James Cadden <jmcadden@bu.edu>
2013-11-07 21:58:06 -0500

    Parser passed through into platform class.
    Cleaned up naming and locations

--- platforms/kh_hpcloud.py
+++ platforms/kh_hpcloud.py
@@ -1,3 +1,5 @@
+from kh_base import *
+
 class KhHpCloud(KhBase):
   def __init__(self):
     None
@@ -5,3 +7,20 @@ class KhHpCloud(KhBase):
   # misc
   def test(self):
     print "You found hp!"
+
+  def parse_get(self, parser):
+    parser.set_defaults(func=self.get)
+    return parser
+  def parse_rm(self, parser):
+    parser.set_defaults(func=self.rm)
+    return parser
+  def parse_setup(self, parser):
+    parser.set_defaults(func=self.setup)
+    return parser
+
+  def get(self):
+    print "hp get"
+  def rm(self):
+    print "hp rm"
+  def setup(self):
+    print "hp setup"
--- platforms/kh_qemu.py
+++ platforms/kh_qemu.py
@@ -1,4 +1,4 @@
-from kh_platform import *
+from kh_base import *
 
 class KhQemu(KhBase):
   def __init__(self):
@@ -7,3 +7,20 @@ class KhQemu(KhBase):
   # misc
   def test(self):
     print "You found qemu!"
+
+  def parse_get(self, parser):
+    parser.set_defaults(func=self.get)
+    return parser
+  def parse_rm(self, parser):
+    parser.set_defaults(func=self.rm)
+    return parser
+  def parse_setup(self, parser):
+    parser.set_defaults(func=self.setup)
+    return parser
+
+  def get(self):
+    print "qemu get"
+  def rm(self):
+    print "qemu rm"
+  def setup(self):
+    print "qemu setup"
