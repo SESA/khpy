3dca593e78fb6bedba6fa3616c3a91a6b314d78e
Jim Cadden <jmcadden@bu.edu>
2015-11-14 16:21:06 -0500

    Adds code cleanup and comments

--- kh_server.py
+++ kh_server.py
@@ -19,39 +19,37 @@ import lockfile
 class KhServerConfig(object):
   def __init__(self, server_ip, server_port, pidfile_path, stdin_path,
       stdout_path, stderr_path):
+    # these are used for the khs py-daemon option
     self.server_ip = server_ip
     self.server_port = server_port
-    touch(pidfile_path)
     self.pidfile_path = pidfile_path
-    touch(stdin_path)
     self.stdin_path = stdin_path
-    touch(stdout_path)
     self.stdout_path = stdout_path
-    touch(stderr_path)
     self.stderr_path = stderr_path
+    touch(pidfile_path)
+    touch(stdin_path)
+    touch(stdout_path)
+    touch(stderr_path)
 
-# Kittyhawk root server object
 class KhServer(object):
   def __init__(self, configsrc):
-    self.config = ConfigParser.SafeConfigParser()
+    self.cfg = ConfigParser.SafeConfigParser()
     khsrc = os.path.dirname(os.path.abspath(__file__))
     dbconfigfile = os.path.join(khsrc,"khdb.cfg")
     configsrc.append(dbconfigfile)
-    self.config.read(configsrc)
-    self.db_path = self.config.get("database","path")
+    self.cfg.read(configsrc)
+    self.db_path = self.cfg.get("database","path")
     self.netpath = os.path.join(self.db_path,
-        self.config.get("BaseDirectories","jobdata"))
+        self.cfg.get("BaseDirectories","jobdata"))
     self.data_node_path = os.path.join(self.db_path,
-        self.config.get("BaseDirectories","nodes"))
+        self.cfg.get("BaseDirectories","nodes"))
     self.data_network_path = os.path.join(self.db_path,
-        self.config.get("BaseDirectories","network"))
+        self.cfg.get("BaseDirectories","network"))
     self.data_job_path = os.path.join(self.db_path,
-        self.config.get("BaseDirectories","job"))
-    # debug info
-    self.debug = self.config.get("debug","debug")
+        self.cfg.get("BaseDirectories","job"))
+    self.debug = self.cfg.get("debug","debug")
 
-  # Default command parsers ##########################################
-
+  # CLI command parsers ##########################################
   def add_parsers(self, subpar):
     # clean
     self.parse_clean(subpar.add_parser('clean',
@@ -81,65 +79,52 @@ class KhServer(object):
     self.parse_stop(subpar.add_parser('stop',
       formatter_class=argparse.ArgumentDefaultsHelpFormatter,
       description="Stop khpy server daemon (if found)"))
-    
   def parse_clean(self, parser):
     parser.set_defaults(func=self.clean)
     return parser
-
   def parse_info(self, parser):
     parser.set_defaults(func=self.info)
     return parser
-
   def parse_install(self, parser):
     parser.set_defaults(func=self.install)
     return parser
-
   def parse_reinstall(self, parser):
     parser.add_argument('-D', 
-            action=KH_store_optional_const, 
-            const=1,
+            action=KH_store_optional_const, const=1,
             help='Start daemon ( req:python-daemon )')
     parser.set_defaults(func=self.reinstall)
     return parser
-
   def parse_restart(self, parser):
     parser.add_argument('-D', 
-            action=KH_store_optional_const, 
-            const=1,
+            action=KH_store_optional_const, const=1,
             help='Restart daemon ( req:python-daemon )')
     parser.set_defaults(func=self.restart)
     return parser
-
   def parse_start(self, parser):
     parser.add_argument('-D', 
-            action=KH_store_optional_const, 
-            const=1,
+            action=KH_store_optional_const, const=1,
             help='Start daemon ( req:python-daemon )')
     parser.set_defaults(func=self.start)
     return parser
-
   def parse_stop(self, parser):
     parser.set_defaults(func=self.stop)
     return parser
 
-  # Client actions ####################################################
-  #     these methods are called remotely and should -eventually- contain
-  #     verification
+  # Client RPC ####################################################
+  #     Client action methods are triggered by client RPC
+  #     naming format <RPC action>_client():
+  #
+  #     TODO: client verification and validation 
 
   def alloc_client(self, jobid, count):
-    ''' Allocate a Node on a Network. Called through client interface '''
-    #TODO verify network
-    #if jobid == None:
-    #  print "Error: network not found"
-    #  exit(1)
-    # create data directory
+    ''' Allocate a node on a clients network. Called through client interface '''
+    #TODO verify network!!!
     datapath = os.path.join(self.db_path,
-        self.config.get("BaseDirectories", "jobdata")+'/'+str(jobid))
+        self.cfg.get("BaseDirectories", "jobdata")+'/'+str(jobid))
     if os.path.exists(datapath) == 0:
       os.mkdir(datapath)
     # get node id
-    nodes = self.db_node_get('*',self.config.get('Settings','FreeJobID'),
-        count)
+    nodes = self.db_node_get('*',self.cfg.get('Settings','FreeJobID'), count)
     # this check should be somewhere smarter..
     if len(nodes) == 0:
       print "Error: not enough free nodes available"
@@ -155,27 +140,25 @@ class KhServer(object):
         os.mkdir(datapath+'/'+str(nid))
     return gotlist
 
-
   def clean_client(self,uid):
     ''' Clean all Nodes and Networks for a particular user '''
-    nets = self.db_net_listbyuid(uid)
+    nets = self.db_netlist_user(uid)
     for name in nets:
       self.remove_network(name)
     return "Your networks and nodes have been removed"
 
-
   def console_client(self, key):
     ''' Console stream to a node '''
+    # todo dump node console to user 
     print "Console support is not yet available."
-
 
   def info_client(self):
     ''' Display all network information for a particualr user'''
+    # TODO return clinets networks and nodes. net:node
     return "Not yet supported"
 
-
   def network_client(self,uid):
-    ''' Allocate a network '''
+    ''' Allocate a network for clientt'''
     jid =  self.db_net_set()
     net_path = os.path.join(self.netpath, str(jid))
     net_uid_path = os.path.join(net_path, "uid")
@@ -187,7 +170,8 @@ class KhServer(object):
           f.truncate()
           f.write(str(uid))
     else:
-      self._print("Error: network "+str(jid)+" already allocated")
+      self._print("Error: network "+str(jid)+" already allocated", sys.stderr)
+      return
     self._print("Allocating network #"+str(jid), sys.stdout)
     return jid
 
@@ -196,22 +180,28 @@ class KhServer(object):
     ## TODO: some sort of user validation
     return self.remove_node(node)
 
-
   def remove_network_client(self, network):
     ''' Remove a network, free nodes. Client validation '''
     ## TODO: some sort of user validation
     return self.remove_network(network)
 
   # Server actions ####################################################
-  # these methods are called directly from the CLI 
+  # these methods are called from the client CLI 
   #
+  # TODO: compatible with a shared db (i.e., a nfs)
+  # TODO: service daemon handles:
+  #                      start (install & start)
+  #                      stop  (clean & stop)
+  #                      restart (start but db maybe active)
+  # FIXME: Should be we able to restart the service without breaking DB state
+  # i.e., a server can be stopped without cleaning nodes and bridges
 
   def clean(self):
     ''' Clean all nodes and networks '''
     print "Cleaning up nodes and networks..."
     if self.server_is_online() == True:
       self.stop()
-    nets = self.db_net_list()
+    nets = self.db_netlist_all()
     # remove all nets
     for name in nets:
       self.remove_network(name)
@@ -223,10 +213,10 @@ class KhServer(object):
     ''' Initilaize the Kittyhawk playform
         Reset counts to default. Reset node and network records '''
     if count == 0:
-      count=self.config.getint("Defaults", "instance_max")
+      count=self.cfg.getint("Defaults", "instance_max")
     # set record for each node
     for i in range(start,start+count):
-      self.db_node_set(i, self.config.get('Settings','FreeJobID'))
+      self.db_node_set(i, self.cfg.get('Settings','FreeJobID'))
     print "Setup complete."
 
   def install(self):
@@ -239,20 +229,20 @@ class KhServer(object):
     if os.path.exists(self.db_path) == 0:
       os.mkdir(self.db_path)
       print self.db_path
-    for s in self.config.options("BaseDirectories"):
-      d = self.config.get("BaseDirectories", s)
+    for s in self.cfg.options("BaseDirectories"):
+      d = self.cfg.get("BaseDirectories", s)
       if os.path.exists(os.path.join(self.db_path, d)) == 0:
         os.mkdir(os.path.join(self.db_path, d))
     # create db files (if needed)
-    for s in self.config.options("BaseFiles"):
-      d = self.config.get("BaseFiles", s)
+    for s in self.cfg.options("BaseFiles"):
+      d = self.cfg.get("BaseFiles", s)
       if os.path.exists(os.path.join(self.db_path, d)) == 0:
         touch(os.path.join(self.db_path, d))
-        with open(self.db_path+'/'+self.config.get('BaseFiles', s), "a") as f:
+        with open(self.db_path+'/'+self.cfg.get('BaseFiles', s), "a") as f:
           f.seek(0)
           f.truncate()
-          f.write(self.config.get('Defaults',s))
-    with open(self.db_path+'/'+self.config.get("BaseFiles","nodeid") ,"r+") as f:
+          f.write(self.cfg.get('Defaults',s))
+    with open(self.db_path+'/'+self.cfg.get("BaseFiles","nodeid") ,"r+") as f:
         start=f.read()
         f.seek(0)
         f.truncate()
@@ -278,11 +268,11 @@ class KhServer(object):
         return 0
     # remove node directory
     datapath = os.path.join(os.path.join(self.db_path,
-               self.config.get("BaseDirectories", "jobdata")),str(netid))
+               self.cfg.get("BaseDirectories", "jobdata")),str(netid))
     nodedatapath = os.path.join(datapath, str(node))
     if os.path.exists(nodedatapath) == 1:
       shutil.rmtree(nodedatapath)
-    self.db_node_set(node, self.config.get('Settings', 'FreeJobID'))
+    self.db_node_set(node, self.cfg.get('Settings', 'FreeJobID'))
     print "Removed node "+str(node)+" from network "+str(netid)
     return "Removed node "+str(node)+" from network "+str(netid)
 
@@ -298,10 +288,10 @@ class KhServer(object):
     # set nodes as free
     for node in nodes:
       nid = node[0:node.find(':')]
-      self.db_node_set(nid, self.config.get('Settings', 'FreeJobID'))
+      self.db_node_set(nid, self.cfg.get('Settings', 'FreeJobID'))
     # remove net data directories
     datapath = os.path.join(os.path.join(self.db_path,
-        self.config.get("BaseDirectories", "jobdata")),str(netid))
+        self.cfg.get("BaseDirectories", "jobdata")),str(netid))
     if os.path.exists(datapath) == 1:
       shutil.rmtree(datapath)
     print "Removing network:",str(netid)
@@ -326,25 +316,27 @@ class KhServer(object):
 
   def server_config(self):
     ''' Server settings must be defined in child class '''
-    self._print("Error: child class must contain server_config() function")
+    self._print("Error: child class must contain server_config() function",
+            sys.stderr)
     exit(1)
     return 0
 
   def start(self, option={}):
     ''' Bring server online
 
-        By default, the server will resume its pervious state, presuming other
-        processes are still runningi externally. Optionally, this function
-        should check if the server had not previous initialized and do so.
+        By default, the server will resume database state, as nodes
+        may still be running externally. 
+        This function also does an install if previous database has 
+        not been initialized.
     '''
-    config = self.server_config()
+    cfg = self.server_config)
     daemon = False
     if option.has_key('D') and option['D'] is 1:
         daemon = True
     print "Starting server..."
     if daemon:
       # aquire lock on pidfile
-      lock = lockfile.FileLock(config.pidfile_path)
+      lock = lockfile.FileLock(cfg.pidfile_path)
       while not lock.i_am_locking():
         try:
           lock.acquire(timeout=3)  # wait up to 3 seconds
@@ -359,15 +351,15 @@ class KhServer(object):
         self.stop()
         exit(1)
       self.daemon_context = daemon.DaemonContext()
-      self.daemon_context.stdin = open(config.stdin_path, 'r')
-      self.daemon_context.stdout = open(config.stdout_path, 'w+')
-      self.daemon_context.stderr = open(config.stderr_path, 'w+', buffering=0)
+      self.daemon_context.stdin = open(cfg.stdin_path, 'r')
+      self.daemon_context.stdout = open(cfg.stdout_path, 'w+')
+      self.daemon_context.stderr = open(cfg.stderr_path, 'w+', buffering=0)
       self.daemon_context.open()
-    with open(config.pidfile_path, "a") as f:
+    with open(cfg.pidfile_path, "a") as f:
       f.seek(0)
       f.truncate()
       f.write(str(os.getpid()))
-    server = SimpleXMLRPCServer((config.server_ip, int(config.server_port)))
+    server = SimpleXMLRPCServer((cfg.server_ip, int(cfg.server_port)))
     server.register_function(self.alloc_client,   "alloc")
     server.register_function(self.console_client, "console")
     server.register_function(self.network_client, "network")
@@ -375,22 +367,22 @@ class KhServer(object):
     server.register_function(self.info_client,    "info")
     server.register_function(self.remove_network_client,"remove_network")
     server.register_function(self.remove_node_client,"remove_node")
-    self._print("Listening on "+config.server_ip+":"+str(config.server_port))
+    self._print("Listening on "+cfg.server_ip+":"+str(cfg.server_port))
     server.serve_forever()
 
   def stop(self):
     ''' Take server offline '''
-    config = self.server_config()
-    lock = lockfile.FileLock(config.pidfile_path)
+    cfg = self.server_config()
+    lock = lockfile.FileLock(cfg.pidfile_path)
     print "Shutting down server..."
     while not lock.i_am_locking():
       try:
         lock.acquire(timeout=3)  # wait up to 3 seconds
       except lockfile.LockTimeout:
         lock.break_lock()
-        if os.path.isfile(config.pidfile_path) == True:
+        if os.path.isfile(cfg.pidfile_path) == True:
           try:
-            pid=int(next(open(config.pidfile_path)))
+            pid=int(next(open(cfg.pidfile_path)))
             self._print("removing server process, pid="+str(pid))
             try:
               os.kill(pid, signal.SIGKILL)
@@ -398,7 +390,7 @@ class KhServer(object):
               self._print("No process to kill. ")
           except StopIteration:
             self._print("No server process found")
-          with open(config.pidfile_path, "a") as f:
+          with open(cfg.pidfile_path, "a") as f:
             f.seek(0)
             f.truncate()
           return 0
@@ -411,7 +403,6 @@ class KhServer(object):
 
   def node_is_valid(self, node):
     ''' Verify that a node is allocated
-
         Return True/False
     '''
     pull = self.db_node_get(node,'*')
@@ -419,13 +410,13 @@ class KhServer(object):
       noderec = pull[0]
       netid = noderec[noderec.find(':')+1:len(noderec)]
       # verify node is assigned to a network
-      if netid is not self.config.get('Settings','FreeJobID'):
+      if netid is not self.cfg.get('Settings','FreeJobID'):
         return True
     return False # no valid node record
 
   def server_is_online(self):
-    config = self.server_config()
-    lock = lockfile.FileLock(config.pidfile_path)
+    cfg = self.server_config()
+    lock = lockfile.FileLock(cfg.pidfile_path)
     while not lock.i_am_locking():
       try:
         lock.acquire(timeout=3)  # wait up to 3 seconds
@@ -454,10 +445,10 @@ class KhServer(object):
     ''' Grab next netid, increment count 
         Always returns int
     '''
-    rid=int(next(open(self.db_path+'/'+self.config.get('BaseFiles','jobid'))))
+    rid=int(next(open(self.db_path+'/'+self.cfg.get('BaseFiles','jobid'))))
     nextid=rid+1
     # increase jobid count
-    with open(self.db_path+'/'+self.config.get('BaseFiles', 'jobid'), "a") as f:
+    with open(self.db_path+'/'+self.cfg.get('BaseFiles', 'jobid'), "a") as f:
       f.seek(0)
       f.truncate()
       f.write(str(nextid))
@@ -475,13 +466,13 @@ class KhServer(object):
     else:
         return None
 
-  def db_net_list(self):
+  def db_netlist_all(self):
     ''' returns list of active networks '''
     return [name for name in os.listdir(self.netpath)
               if os.path.isdir(os.path.join(self.netpath, name))]
 
-  def db_net_listbyuid(self, uid):
-    nets = self.db_net_list()
+  def db_netlist_user(self, uid):
+    nets = self.db_netlist_all()
     ret = []
     for name in nets:
       uid_file = os.path.join(os.path.join(self.netpath, name), "uid")
