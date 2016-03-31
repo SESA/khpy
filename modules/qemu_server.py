9f88f9a703ce953e5aca6997e1d2a5c4efaedde2
Jim Cadden <jmcadden@bu.edu>
2016-03-31 18:26:22 -0400

    fix(QemuServer): correctly prepend perf command

--- modules/qemu_server.py
+++ modules/qemu_server.py
@@ -153,7 +153,8 @@ vectors="+str((2*int(netcpu))+2)+",netdev=vlan1,mac="+mac
       if option.has_key('perf')  :
         perf_cmd = self.config.get('Qemu','perf_cmd')+" -o "+nodedir+"/perf "
         if len(option['perf']) > 0:
-          perf_cmd += option['perf']
+            perf_cmd += " " 
+            perf_cmd += option['perf']
         cmd = "( "+perf_cmd+" "+cmd+" ) </dev/null &"
         ret += nodedir+"/perf\n"
       else:
